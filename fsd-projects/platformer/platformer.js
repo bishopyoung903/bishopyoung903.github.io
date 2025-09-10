3e06dbad27a96cbbeb404ff52a80cee9b5a51430
bishopyoung903 <bishopyoung903@gmail.com>
2025-09-10 15:41:25 +0000

    images

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,44 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+   //TOGGLEGRID);
 
 
     // TODO 2 - Create Platforms
+    createPlatform(500, 0, 20, 290);
+createPlatform(200, 270, 20, 20, );
+createPlatform(500, 1, 20, 400);
+createPlatform(100, 640, 20, 20, );
+createPlatform(180, 600, 20, 20, );
+createPlatform(300, 550, 20, 20, );
+createPlatform(200, 420, 50, 15,)
+createPlatform(100, 350, 20, 20, );
+createPlatform(500, 550, 20, 200)
+createPlatform(600 ,520,20, 20,)
+createPlatform(700, 480, 20,20)
+createPlatform(580, 370, 75, 20)
+createPlatform(750, 420, 20 , 20)
+createPlatform(610, 210, 20, 20)  
+createPlatform(850, 180, 75, 20)
+createPlatform(720, 310, 20 , 20)
 
 
 
 
     // TODO 3 - Create Collectables
+    createCollectable("steve", 1000, 200);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
+createCollectable("steve", 600,500)
 
 
 
     
     // TODO 4 - Create Cannons
+    createCannon("top", 300, 4000)
+createCannon("right", 300, 4000)
+createCannon("right", 700,  4000)
+createCannon("right", 730, 4000)
+createCannon("right", 440,4000);
 
 
     
